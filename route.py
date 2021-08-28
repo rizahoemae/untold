efcc29e92c155ed055ce88e678f2c74af094cc86
rizahoemae <humaida.mawaddatur@gmail.com>
2021-08-28 23:59:19 +0700

    initial commit

--- route.py
+++ route.py
@@ -33,12 +33,23 @@ def index():
     query_read = query_db("select * from story order by created_at desc")
     return render_template("index.html", story=query_read)
 
-@app.route('/confess', methods=['POST'])
+@app.route('/confess', methods=['GET','POST'])
 def confess():
-    if request.method == 'POST':
-        title_story = request.form['title_story']
-        username = request.form['username']
-        stories = request.form['stories']
-        query_create = query_db("insert into story(title_story, username, stories) values (%s, %s, %s)", (title_story, username, stories))
-        return redirect(url_for('index'))
+    title_story = request.form['title_story']
+    username = request.form['username']
+    stories = request.form['stories']
+    query_create = query_db("insert into story(title_story, username, stories) values (%s, %s, %s)", (title_story, username, stories))
+    return redirect(url_for('index'))
+
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    if request.method == "POST":
+        query = request.form.get('cari')
+        query_search = query_db("select title_story, username, stories from story where title_story like %s or username like %s or stories like %s", (query, query, query))
+        anu = query_search.fetchall()
+        return render_template("result.html", records=query_search.fetchall()).format(query)
         
+@app.route('/result/query', methods=['GET','POST'])
+def success(cari):
+    
+    return render_template("result.html", cari=query_search)
